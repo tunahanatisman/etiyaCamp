800fe6e7bc319397106d3c3aef03453810199874
Tunahan Atışman <67320832+tunahanatisman@users.noreply.github.com>
2022-07-22 15:33:33 +0300

    Update perfectNumber.js

--- src/homework2/perfectNumber.js
+++ src/homework2/perfectNumber.js
@@ -1,6 +1,4 @@
-//Mükemmel sayı, sayılar teorisinde, kendisi hariç pozitif tam bölenlerinin toplamı 
-//kendisine eşit olan sayı. 
-//Diğer bir ifadeyle, bir mükemmel sayı, bütün pozitif tam bölenlerinin toplamının yarısına eşittir.
+
 function perfectNumber() {
 
     for (let j = 1; j < 1000; j++) {
